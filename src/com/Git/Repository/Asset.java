0183c91f39f4cf4f89dd3122481ab55ab7484af2
Liviu Muraru <liviu.muraru.97@gmail.com>
2018-05-11 01:06:16 +0300

    added: - add Asset generic class; represents a GitHub asset - add Assets static class for static methods that work with/on Assets

---
+++ src/com/Git/Repository/Asset.java
@@ -0,0 +1,26 @@
+package com.Git.Repository;
+
+import java.io.File;
+
+public class Asset<T>
+{
+    private T internal_asset;
+
+    private Asset()
+    {
+
+    }
+
+    public T getAsset()
+    {
+        return internal_asset;
+    }
+
+    public static Asset<File> FromFile(File file)
+    {
+        Asset ast = new Asset();
+        ast.internal_asset = file;
+
+        return ast;
+    }
+}
---
+++ src/com/Git/Repository/Assets.java
@@ -0,0 +1,42 @@
+package com.Git.Repository;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Assets
+{
+    public static File MergeFiles(List<Asset<File>> fileList, Path destination) throws IOException {
+        File mergedFiles = destination.toFile();
+        mergedFiles.createNewFile();
+        BufferedWriter bw = new BufferedWriter(new FileWriter(mergedFiles));
+
+        for (Asset<File> fileAsset : fileList)
+        {
+            BufferedReader br = new BufferedReader(new FileReader(fileAsset.getAsset()));
+            String text = null;
+            bw.write(fileAsset.getAsset().getAbsolutePath());
+            bw.newLine();
+            while ((text = br.readLine()) != null)
+            {
+                System.out.println(text);
+                bw.write(text);
+                bw.newLine();
+            }
+            br.close();
+        }
+        bw.close();
+
+        //print merge file
+        BufferedReader br = new BufferedReader(new FileReader(mergedFiles));
+        String text = null;
+        while ((text = br.readLine()) != null)
+        {
+        //    System.out.println(text); ????
+                bw.write(text);
+                bw.newLine();
+        }
+
+        return mergedFiles;
+    }
+}

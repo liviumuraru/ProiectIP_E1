d0839292b76a29fbc84998d9bdef33ba43a0cb15
Liviu Muraru <liviu.muraru.97@gmail.com>
2018-05-13 13:25:34 +0300

    added: - JavaFilesOnly premade filter, responsible of filtering for ".java" files

---
+++ src/com/Git/Repository/Generics/Premade/Filter/JavaFilesOnly.java
@@ -0,0 +1,36 @@
+package com.Git.Repository.Generics.Premade.Filter;
+
+import com.Git.Repository.Asset;
+import com.Git.Repository.Generics.Filter;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaFilesOnly implements Filter<Asset<File>>
+{
+    @Override
+    public List<Asset<File>> accept(List<Asset<File>> input)
+    {
+        List<Asset<File>> newFiles = new ArrayList<>();
+        for(Asset<File> file : input){
+            int indexOfDot = file.getAsset().getName().indexOf
+                    (".");
+            if(!file.getAsset().isDirectory() && indexOfDot != -1)
+            {
+                String extension = file.getAsset().getName().substring(indexOfDot + 1,
+                        file
+                                .getAsset()
+                                .getName()
+                                .length());
+
+                System.out.println(extension);
+
+                if(extension.equals("java"))
+                    newFiles.add(file);
+            }
+        }
+
+        return newFiles;
+    }
+}

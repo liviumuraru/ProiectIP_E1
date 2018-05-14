a75e5b5a436cf6e5f676dcdcd28727c9ac467bc9
leryss <laur.h_97@yahoo.ro>
2018-05-14 20:07:46 +0300

    added python script filtering

---
+++ GH/src/filters/PyFilesOnly.java
@@ -0,0 +1,33 @@
+package filters;
+
+
+import asset.Asset;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PyFilesOnly implements Filter< Asset<File> >
+{
+	static final String[] possibleExtensions = { "py", "py3", "pyw", "pyx"};
+    @Override
+    public List<Asset<File>> accept(List<Asset<File>> input){
+        List<Asset<File>> newFiles = new ArrayList<>();
+        for(Asset<File> file : input){
+            int indexOfDot = file.getAsset().getName().indexOf(".");
+            if(!file.getAsset().isDirectory() && indexOfDot != -1){
+                String extension = file.getAsset().getName().substring(indexOfDot + 1, file.getAsset().getName().length());
+                System.out.println(extension);
+
+                for(String ext : possibleExtensions) {
+                    if(extension.equals(ext)) {
+                    	newFiles.add(file);
+                    	break;
+                    }                        
+                }
+            }
+        }
+
+        return newFiles;
+    }
+}

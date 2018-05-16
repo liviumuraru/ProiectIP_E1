aae1dde86d7cc4a697446f44b4f90296d22b3984
Laurentiu Horghidan <lhorghidan@bitdefender.com>
2018-05-16 09:03:54 +0300

    fixed file reading bug from indexer

--- GH/src/lucene/Indexer.java
+++ GH/src/lucene/Indexer.java
@@ -14,6 +14,7 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.apache.lucene.analysis.Analyzer;
@@ -93,10 +94,10 @@ public class Indexer {
      */
     public String getAllText(File f) throws IOException {
         String textFileContent = "";
+        for (String line : Files.readAllLines(Paths.get(f.getAbsolutePath()), StandardCharsets.ISO_8859_1)) {
+        	textFileContent += line;
+        }        	
 
-        for (String line : Files.readAllLines(Paths.get(f.getAbsolutePath()))) {
-            textFileContent += line;
-        }
         return textFileContent;
     }
 }

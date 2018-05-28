423cfc9a320bb007be37db0d6cf5709eab372b36
Mihai-mancas <slayers_mihai@yahoo.com>
2018-05-29 00:42:29 +0300

    v_0.2 - tree by ordering criteria
    v_0.2 - tree by ordering criteria

--- M_prime/M_prime/src/sorting/Main.java
+++ M_prime/M_prime/src/sorting/Main.java
@@ -1,7 +1,5 @@
 package sorting;
 
-import java.awt.Color;
-
 import javax.swing.*;
 
 public class Main {
@@ -9,12 +7,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         PanelList frame = new PanelList ();
+        frame.setVisible ( true );
         //frame.setBackground(Color.LIGHT_GRAY);
         frame.setSize(820,680);
         
         frame.setLocationRelativeTo(null);
         //frame.pack();
-        frame.setVisible ( true );
+
         frame.setDefaultCloseOperation ( WindowConstants.EXIT_ON_CLOSE );
 
 
--- M_prime/M_prime/src/sorting/PanelList.java
+++ M_prime/M_prime/src/sorting/PanelList.java
@@ -3,16 +3,20 @@ package sorting;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
+import java.util.Vector;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static java.lang.Thread.sleep;
 
 class PanelList extends JFrame {
 
     private JCheckBox box1 = new JCheckBox("Number of Contributor Followers");
     private JCheckBox box2 = new JCheckBox("Number of Forks");
     private JCheckBox box3 = new JCheckBox("Number of Releases");
-//    private JCheckBox box4 = new JCheckBox("Criterion 4");
+    //    private JCheckBox box4 = new JCheckBox("Criterion 4");
 
     PanelList() {
-    	
+
         JPanel panelUp = new JPanel();
         panelUp.add(new JLabel("<html><font color='white'>These are the results</font></html>"));
         this.add(panelUp, BorderLayout.NORTH);
@@ -24,20 +28,102 @@ class PanelList extends JFrame {
         panelLeft.setBackground(Color.LIGHT_GRAY);
         JButton button = new JButton("<html><font color='black'>Sort</font></html>");
         button.setBackground(Color.white);
-        
+
         panelLeft.add(button);
         this.add(panelLeft, BorderLayout.EAST);
 
+        /**
+         * name of the root file where the folders are
+         */
 
-        FileTree model = new FileTree(new File("M_Prime"));
+        File sit = new File("C:\\Users\\Dell\\Desktop\\Computer Networks");
 
-        this.add(model, BorderLayout.CENTER);
+        String[] dir = sit.list();
+        Vector<String> onlydir = new Vector<>();
+        for (String s :
+                dir) {
+            if (new File(sit.getAbsolutePath() + File.separator + File.separator + s).isDirectory()) {
+                onlydir.add(sit.getAbsolutePath() + File.separator + File.separator + s);
+            }
 
+        }
+
+        AtomicReference<FileTree> model = new AtomicReference<>();
+
+
+
+        model.set(new FileTree(sit, onlydir));
+        this.add(model.get(), BorderLayout.CENTER);
         button.addActionListener(e -> {
 
             // Get new tree based on the checkbox criteria
+            this.remove(model.get());
 
+            /**
+             * @param rand Vector from Sorting Component
+             *
+             */
+
+            Vector<String> rand = new Vector<>();
+
+            if (box1.isSelected()) {
+                // call method for first sort
+
+                rand.add(onlydir.elementAt(1));
+                rand.add(onlydir.elementAt(2));
+                rand.add(onlydir.elementAt(3));
+                rand.add(onlydir.elementAt(4));
+                rand.add(onlydir.elementAt(5));
+
+                model.set(new FileTree(sit, rand));
+
+            }
+
+            if (box2.isSelected()) {
+                // call method for second sort
+                rand.add(onlydir.elementAt(5));
+                rand.add(onlydir.elementAt(4));
+                rand.add(onlydir.elementAt(3));
+                rand.add(onlydir.elementAt(2));
+                rand.add(onlydir.elementAt(1));
+
+                model.set(new FileTree(sit, rand));
+
+            }
+
+            if (box3.isSelected()) {
+                // call method for third sort
+                rand.add(onlydir.elementAt(4));
+                rand.add(onlydir.elementAt(3));
+                rand.add(onlydir.elementAt(5));
+                rand.add(onlydir.elementAt(1));
+                rand.add(onlydir.elementAt(2));
+
+                model.set(new FileTree(sit, rand));
+            }
+            this.add(model.get(), BorderLayout.CENTER);
+            this.revalidate();
         });
+
+        box1.addActionListener(e -> {
+            if (box1.isSelected()) {
+                box2.setSelected(false);
+                box3.setSelected(false);
+            }
+        });
+        box2.addActionListener(e -> {
+            if (box2.isSelected()) {
+                box1.setSelected(false);
+                box3.setSelected(false);
+            }
+        });
+        box3.addActionListener(e -> {
+            if (box3.isSelected()) {
+                box1.setSelected(false);
+                box2.setSelected(false);
+            }
+        });
+
 
         box1.setBackground(Color.LIGHT_GRAY);
         box2.setBackground(Color.LIGHT_GRAY);

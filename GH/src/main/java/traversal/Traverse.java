14d5a782a8f7c41c6dbab36ed2835835ef7c0f4a
oana-hriscu <oana.hriscu97@gmail.com>
2018-05-30 00:36:06 +0300

    Update Traverse.java

--- GH/src/main/java/traversal/Traverse.java
+++ GH/src/main/java/traversal/Traverse.java
@@ -5,27 +5,32 @@ import java.io.IOException;
 
 import concatenation.*;
 
+/**
+ * Class Traverse is used to access every file and directory existing in a given path
+ */
+
 public class Traverse {
 
-        static Concat c = new Concat();
-        public static void main (String args[]) throws IOException {
+    static Concat c = new Concat();
+    public static void main (String args[]) throws IOException {
 
-            displayIt(new File("D:\\Anul 2\\ProiectIP\\ProiectIPM3"));
-        }
+        displayIt(new File("D:\\Anul 2\\ProiectIP\\ProiectIPM3"));
+    }
 
-        public static void displayIt(File node) throws IOException {
-
-
-
-            if(node.isDirectory() && !node.getName().startsWith(".")){
-//                System.out.println(node.getAbsoluteFile());
-                String[] subNote = node.list();
-                for(String filename : subNote){
-//                    System.out.println(filename);
-                    displayIt(new File(node, filename));
-                    c.run(node.getAbsoluteFile());
-                }
+    /**
+     * Parameter node represents the given path. Files and directories are recursively accessed and the run method
+     * belonging to class Concat is called when a file is reached
+     * @param node
+     * @throws IOException
+     */
+    public static void displayIt(File node) throws IOException {
+        if(node.isDirectory() && !node.getName().startsWith(".")){
+            String[] subNote = node.list();
+            for(String filename : subNote){
+                displayIt(new File(node, filename));
+                c.run(node.getAbsoluteFile());
             }
-            else return;
         }
+        else return;
+    }
 }

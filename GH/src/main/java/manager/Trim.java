86b894966414ecada41b0b167e75e151e749a62c
oana-hriscu <oana.hriscu97@gmail.com>
2018-05-30 00:47:26 +0300

    Update Trim.java

--- GH/src/main/java/manager/Trim.java
+++ GH/src/main/java/manager/Trim.java
@@ -8,29 +8,68 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Class Trim follows the Singleton pattern
+ */
 public class Trim
 {
+    private static Trim single_instance = new Trim();
     private static Map<String, GHRepository > allProjects = new HashMap<>();
     private static Map<GHRepository, String> paths = new HashMap<>();
     private static List<String> remainingProjects = new ArrayList<>();
 
+    /**
+     * Private constructor restricted to this class itself
+     */
+    private Trim() {}
+
+    /**
+     * Static method to create instance of Singleton class
+     * @return
+     */
+    public static Trim getInstance() {
+        return single_instance;
+    }
+
+    /**
+     * Adds path to the list of all paths
+     * @param ghRepository
+     * @param path
+     */
     public static void addPath( GHRepository ghRepository, String path ) { paths.put( ghRepository, path ); }
 
+    /**
+     * Adds projects to the list of all projects
+     * @param path
+     * @param ghRepository
+     */
     public static void add(String path, GHRepository ghRepository)
     {
         allProjects.put( path, ghRepository );
     }
 
+    /**
+     * Adds remaining projects
+     * @param remainingProject
+     */
     public static void addRemainingProject( String remainingProject )
     {
         remainingProjects.add( remainingProject );
     }
 
+    /**
+     * Getter for the list of all projects
+     * @return
+     */
     public static List<GHRepository> getRepos()
     {
         return new ArrayList<>( allProjects.values() );
     }
 
+    /**
+     * Getter for remaining projects
+     * @return
+     */
     public static List< Repo > getRemaining()
     {
         if(remainingProjects.isEmpty())
@@ -47,6 +86,11 @@ public class Trim
         return remaining;
     }
 
+    /**
+     * Getter for list of paths
+     * @param ghRepositories
+     * @return
+     */
     public static List<String> getPaths( List<GHRepository> ghRepositories )
     {
         List<String> sortedPaths = new ArrayList<>();
@@ -58,3 +102,4 @@ public class Trim
         return sortedPaths;
     }
 }
+

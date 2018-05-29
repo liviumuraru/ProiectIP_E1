2d6b6ffa6057dfc86d149ed53c2ff3c9c56e39c0
oana-hriscu <oana.hriscu97@gmail.com>
2018-05-30 00:12:58 +0300

    Update RunScript.java

--- GH/src/main/java/reverse/RunScript.java
+++ GH/src/main/java/reverse/RunScript.java
@@ -2,7 +2,17 @@ package reverse;
 
 import java.io.IOException;
 
+/**
+ * The class RunScript runs python scripts from command prompt with given arguments;
+ * Said action is executed by the run method
+ */
+
 public class RunScript {
+    /**
+     * Parametres path and arguments are given and used in command prompt
+     * @param path
+     * @param arguments
+     */
     public static void run(String path, String...arguments) {
         try {
             Process p = Runtime.getRuntime().exec("python " + path + " "

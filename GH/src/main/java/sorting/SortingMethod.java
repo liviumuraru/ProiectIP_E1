f52a4232a6e1c8dde306f684afc03fad4d90c10b
andrachiorcea <achiorcea27@gmail.com>
2018-05-30 08:14:28 +0300

    Update SortingMethod.java

--- GH/src/main/java/sorting/SortingMethod.java
+++ GH/src/main/java/sorting/SortingMethod.java
@@ -6,6 +6,14 @@ import org.kohsuke.github.GHRepository;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * This abstract class lets you choose the desired sorting method
+ */
 public abstract class SortingMethod {
+      /**
+     * Returns a list of GHRepository objects sorted in an order specified by the classes extending this abstract class.
+     * @param  repositories
+     * a list of Repo objects (which contain a GHRepository and a path)
+     */
     public abstract List<GHRepository> sort(List<Repo> repositories);
 }

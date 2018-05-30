eb5b85ba402d4263e9737f9bb83aa49a278fa5da
andrachiorcea <achiorcea27@gmail.com>
2018-05-30 08:13:39 +0300

    Update SortingFactory.java

--- GH/src/main/java/sorting/SortingFactory.java
+++ GH/src/main/java/sorting/SortingFactory.java
@@ -4,11 +4,19 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+ * This class keeps a map where for each type of sorting we have associated a string (describing the sorting and acting as a key)
+ * and an object (from a class extending the abstract class SortingMethod) which handles the sorting.
+ */
 public class SortingFactory {
 
     private Map<String, SortingMethod> sortingMethods = new HashMap<>();
 
-
+    /**
+    * Verifies if the type of sorting specified by the string is possible (exists in the map or not). If not,
+     * it throws an "Incorrect sorting method" IOException, else it returns the value associated to that key in the map
+     * @param sortingMethod
+     */
     public SortingMethod getSortingMethod(String sortingMethod) {
         try
         {

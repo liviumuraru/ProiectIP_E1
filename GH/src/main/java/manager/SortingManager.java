6eede179653df49c3b29d5de03c4c288a9090515
andrachiorcea <achiorcea27@gmail.com>
2018-05-30 08:16:26 +0300

    Update SortingManager.java

--- GH/src/main/java/manager/SortingManager.java
+++ GH/src/main/java/manager/SortingManager.java
@@ -15,20 +15,33 @@ public class SortingManager {
     private SortingFactory sortingFactory = new SortingFactory();
     private List<Repo> repositoryList = getRemaining();
 
+     /**
+     * Adds the type of sorting available to the sortingFactory object
+     */
     public SortingManager() {
         sortingFactory.AddSortingMethod("SortByNumberOfContributorFollowers", new SortByNumberOfContributorFollowers());
         sortingFactory.AddSortingMethod("SortByNumberOfForks", new SortByNumberOfForks());
         sortingFactory.AddSortingMethod("SortByNumberOfReleases", new SortByNumberOfReleases());
     }
 
+     /**
+     * Returns the sorting method corresponding to the stringMethod attribute
+     */
     public SortingMethod getSortingMethod() {
         return sortingFactory.getSortingMethod(stringMethod);
     }
 
+     /**
+     * Returns the stringMethod attribute
+     */
     public String getStringMethod() {
         return stringMethod;
     }
 
+     /**
+     * Sets the value of the stringMethod attribute
+     * @param stringMethod
+     */
     public void setStringMethod(String stringMethod) {
         this.stringMethod = stringMethod;
     }
